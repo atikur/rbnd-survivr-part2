aa160e0ebbadca27997bf328b54c7744f984c330
Atikur Rahman <atik.bd08@gmail.com>
2016-02-20 01:16:11 +0600

    pass TestJury tests

--- lib/jury.rb
+++ lib/jury.rb
@@ -3,9 +3,35 @@ class Jury
 
   def initialize
   	@members = []
+  	@votes = {}
   end
 
   def add_member(name)
   	@members.push(name)
+  end
+
+  def cast_votes(finalists)
+  	votes = {finalists[0] => 0, finalists[1] => 0}
+
+  	@members.count.times.each do |i|
+  	  if rand(0...2) == 0
+  	  	votes[finalists[0]] += 1
+  	  else
+  	  	votes[finalists[1]] += 1
+  	  end
+  	  puts "vote by member #{i}"
+  	end
+
+  	votes
+  end
+
+  def report_votes(votes)
+  	votes.each do |key, val|
+  	  puts "#{key} - #{val}"
+  	end
+  end
+
+  def announce_winner(votes)
+  	votes.key(votes.values.max)
   end
 end
